1e6f6718745db8e5055a5f36d01308c1fa442ea6
p4cm4n972 <manuel.adele@gmail.com>
2016-12-18 14:37:32 +0100

    sass

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ function initialize() {
         function(position) {
             var mapOptions = {
                 center: { lat: position.coords.latitude, lng: position.coords.longitude},
-                zoom: 8
+                zoom: 16
             };
         var map = new google.maps.Map(document.getElementById('maps-canvas'),mapOptions);
         }
